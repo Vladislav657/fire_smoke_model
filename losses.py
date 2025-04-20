cfcd012c5c721541d7e0610e1197068c60c01b56
vlad <ecscalibur_10@mail.ru>
2025-04-20 13:54:28 +0300

    trained model

--- losses.py
+++ losses.py
@@ -2,17 +2,26 @@ import torch.nn as nn
 
 
 class SoftDiceLoss(nn.Module):
-    def __init__(self, smooth=1):
+    def __init__(self, smooth=1.0):
         super().__init__()
         self.smooth = smooth
 
     def forward(self, logits, targets):
-        num = targets.size(0)
-        probs = nn.functional.sigmoid(logits)
-        m1 = probs.view(num, -1)
-        m2 = targets.view(num, -1)
-        intersection = (m1 * m2)
+        batch_size, num_classes = logits.shape[0], logits.shape[1]
 
-        score = 2 * (intersection.sum(1) + self.smooth) / (m1.sum(1) + m2.sum(1) + self.smooth)
-        score = 1 - score.sum() / num
-        return score
+        # Применяем softmax по классам для получения вероятностей
+        probs = nn.functional.softmax(logits, dim=1)
+
+        # Подготавливаем тензоры: [batch_size, num_classes, -1]
+        probs_flat = probs.view(batch_size, num_classes, -1)
+        targets_flat = targets.view(batch_size, num_classes, -1)
+
+        # Вычисляем пересечение и суммы для каждого класса
+        intersection = (probs_flat * targets_flat).sum(2)  # [batch_size, num_classes]
+        cardinality = (probs_flat + targets_flat).sum(2)  # [batch_size, num_classes]
+
+        # Вычисляем Dice score для каждого класса
+        dice_scores = (2. * intersection + self.smooth) / (cardinality + self.smooth)  # [batch_size, num_classes]
+        dice_scores = dice_scores.mean(1)
+        dice_loss = 1. - dice_scores.mean()  # [batch_size]
+        return dice_loss
